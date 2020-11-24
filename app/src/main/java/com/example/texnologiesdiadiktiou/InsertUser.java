17a51588251f8bc23284827d112d2d09b62ba815
terebravisse <gnarishtnt@gmail.com>
2020-11-24 20:32:19 +0200

    Project update

---
+++ app/src/main/java/com/example/texnologiesdiadiktiou/InsertUser.java
@@ -0,0 +1,14 @@
+package com.example.texnologiesdiadiktiou;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert_user);
+    }
+}
